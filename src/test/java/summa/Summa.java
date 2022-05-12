ef158e9ca91091ee052cea960a526eb67d41d80f
boopathypradeep <boopathypradeepr@gmail.com>
2022-05-12 08:21:20 +0530

    my updates

---
+++ src/test/java/summa/Summa.java
@@ -0,0 +1,21 @@
+package summa;
+
+public class Summa {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hai");
+		System.out.println("hello");
+		System.out.println("hai");
+		System.out.println("hello");
+		System.out.println("hai");
+		System.out.println("hello");
+		System.out.println("hai");
+		System.out.println("hello");
+		System.out.println("hai");
+		System.out.println("hello");
+		System.out.println("hai");
+		System.out.println("hello");
+		System.out.println("hai");
+	}
+
+}
